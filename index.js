416417b4bbaed8a140a036541cdec12c40d768ab
kitsada <joe.kidsada.02740@gmail.com>
2019-01-21 12:50:38 +0700

    add script start node and edit port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.POST || 5000
+const port = process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
+ 
 app.get('/',(req, res) => {
     res.send({status: 'ok'})
 })
