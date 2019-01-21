f71aaeb25d46f19eda05539896c062aff855d2c2
kitsada <joe.kidsada.02740@gmail.com>
2019-01-21 12:33:01 +0700

    โจ

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.POST || 5000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/',(req, res) => {
+    res.send({status: 'ok'})
+})
+
+app.post('/webhook',(req, res) => {
+    let body = req.body;
+    let response = { 
+        status: 'ok',
+        body: body
+    }
+    console.log("Body ==> ")
+    console.log(body);
+    res.send(response);
+})
+
+app.listen(port, () => {
+    console.log(`server run on port ${port}`);
+})
